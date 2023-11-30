06c7b8c29ad976dbf31dc3b346a524804128e2e3
NevilleSFU <ntl11@sfu.ca>
2023-11-29 17:58:59 -0800

    Adding a geocoding API calls

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home.js';
+import Result from './pages/result.js';
 
 function Main() {
     return (
         <div>
-            <Home/>
+            <Routes>
+                <Route path='/' element={<Home/>}></Route>
+                <Route path='/result' element={<Result/>}></Route>
+            </Routes>
         </div>
     );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import {useState} from 'react';
 import Header from '../components/header.js';
 import search_icon from '../assets/search_icon.png';
 import './home.css';
 
 function Home() {
+    const [searchval, setSearch] = useState();
+    function handleChange(event) {
+        setSearch(event.target.value);
+    }
+
+    async function search() {
+        try {
+            var locationstring = searchval.trim();
+            locationstring = locationstring.replace(" ", "%20");
+            console.log(locationstring);
+
+            fetch("https://us1.locationiq.com/v1/search?key=pk.5656ae4179330b525702ad97ed3ba00e\
+            &format=json&q=" + locationstring)
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((geolocation) => {
+                // an array of json objects are returned
+                // display all of them on result page
+                console.log(geolocation);
+                console.log(geolocation[0]["display_name"]);
+            });
+        } catch (error) {
+            console.log("Error: API went wrong");
+            console.log(error);
+        }
+    }
+
     return (
         <div style={{textAlign: "center", overflowX: "hidden"}}>
             <Header/>
@@ -11,8 +41,13 @@ function Home() {
                 <section className='infobox'>
                     <h2>Welcome!</h2>
                     <div>
-                        <input type="text" placeholder='Search a place to get started:'></input>
-                        <img src={search_icon} width="40px" height="40px"></img>
+                        <input id="input"
+                        type="text" 
+                        placeholder='Search a place to get started:'
+                        onChange={handleChange}></input>
+                        <button onClick={search}>
+                            <img src={search_icon} width="40px" height="40px"></img>
+                        </button>
                     </div>
                 </section>
             </div>
