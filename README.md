e181337f88baf2befa446725fc1560f8424baeb1
NevilleSFU <ntl11@sfu.ca>
2023-12-03 19:23:13 -0800

    Revert "Updates"
    This reverts commit 818f359df5ae215438ae237a359386922583eac0.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Location_Searcher
+Planned features:
+- current weather display
+- display climate
+- display currency
+- search for flights
+
+# API Used
+Used for searching location and geocoding
+https://locationiq.com/demo
+For getting weather forecast
+https://open-meteo.com/en/docs#current=
+For getting country information
+https://restcountries.com/
+To get currency information
+https://freecurrencyapi.com/
+
+# Build Instructions
+1. Run "npm install" and "npm install chart.js react-chartjs-2" if necessary
+2. Run "npm start" to host locally
+3. To build, run "npm run build"
+
---
+++ src/components/searchresult.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './searchresult.css';
+
+function Searchcard(props) {
+    const {data} = props;
+    return (
+        <Link style={{ textDecoration: 'none', color: 'inherit' }}
+        to={"/result/"+ data["display_name"].replaceAll(" ", "_") + "&" +
+        parseFloat(data["lat"]).toFixed(2)+"&"+parseFloat(data["lon"]).toFixed(2)}>
+            <div className="searchcard">
+                <p className="card_text">{data["display_name"]}</p>
+                <p className="sm_card_text"> Latitude: {parseFloat(data["lat"]).toFixed(2)}</p>
+                <p className="sm_card_text"> Longitude: {parseFloat(data["lon"]).toFixed(2)}</p>
+            </div>
+        </Link>
+    );
+}
+
+export default Searchcard;
