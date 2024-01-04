b3a5975ddb0e45ef48bcaf8e7daf91dcfc14ee5f
NevilleSFU <ntl11@sfu.ca>
2024-01-04 13:13:19 -0800

    Trying

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {HashRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <p>Basics</p>
     <HashRouter base="/">
       <Main/>
     </HashRouter>
